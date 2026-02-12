3cd487105b97a504411bc362ceafddcd83d0f24c
Your Name <Sofiaanda26@outlook.com>
2026-02-12 11:04:26 +0800

    fix: 追加

--- tests/e2e/run_gsm8k_fsdp_sgl_multiturn_w_tool_2gpu.sh
+++ tests/e2e/run_gsm8k_fsdp_sgl_multiturn_w_tool_2gpu.sh
@@ -22,6 +22,7 @@ FSDP_STRATEGY=${FSDP_STRATEGY:-fsdp}
 python3 -m verl.trainer.main_ppo \
     --config-path="$CONFIG_PATH" \
     --config-name='gsm8k_multiturn_grpo' \
+    env.env_name=gsm8k \
     algorithm.adv_estimator=grpo \
     data.train_batch_size=64 \
     data.max_prompt_length=1024 \
