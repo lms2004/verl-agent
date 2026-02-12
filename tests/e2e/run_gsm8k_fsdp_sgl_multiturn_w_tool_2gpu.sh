1c4cb171e78dbfa6439b8a35c71551e3ac707f7e
Your Name <Sofiaanda26@outlook.com>
2026-02-12 11:07:59 +0800

    fix: In verl+env, we keep n=1, and achieve GRPO by env.rollout.n

--- tests/e2e/run_gsm8k_fsdp_sgl_multiturn_w_tool_2gpu.sh
+++ tests/e2e/run_gsm8k_fsdp_sgl_multiturn_w_tool_2gpu.sh
@@ -47,7 +47,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=sglang \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.7 \
-    actor_rollout_ref.rollout.n=6 \
+    actor_rollout_ref.rollout.n=1 \
+    env.rollout.n=6 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=16 \
     actor_rollout_ref.ref.strategy=$FSDP_STRATEGY \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
--- tests/e2e/run_gsm8k_fsdp_sgl_multiturn_w_tool_single_gpu.sh
+++ tests/e2e/run_gsm8k_fsdp_sgl_multiturn_w_tool_single_gpu.sh
@@ -47,7 +47,8 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
     actor_rollout_ref.rollout.name=sglang \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.85 \
-    actor_rollout_ref.rollout.n=4 \
+    actor_rollout_ref.rollout.n=1 \
+    env.rollout.n=4 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.ref.strategy=$FSDP_STRATEGY \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
